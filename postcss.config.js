ceac2eba10ff213701d4cafb6627e08ed957f316
Sunset-kim <wood2002@naver.com>
2022-01-17 00:55:02 +0900

    ➕ install : uuid, framer motion

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('@tailwindcss/forms'),
     require('postcss-import'),
-    require('tailwindcss/nesting'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
     require('tailwindcss'),
     require('autoprefixer'),
   ],
