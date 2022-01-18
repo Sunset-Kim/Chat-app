13c3ba9618ef453dd42333a470e0d5c79d51746b
Sunset-kim <wood2002@naver.com>
2022-01-18 23:08:47 +0900

    🎨  add : 그림 자막 2줄 넣기

--- src/components/canvas/canvas.tsx
+++ src/components/canvas/canvas.tsx
@@ -1,4 +1,3 @@
-import { async } from '@firebase/util';
 import React, {
   ReactEventHandler,
   useState,
@@ -14,6 +13,7 @@ import { getDownloadURL } from 'firebase/storage';
 import { useRecoilValue } from 'recoil';
 import { userIdAtom } from '../../state/auth';
 import StoreService from '../../services/fire_store';
+
 const Canvas = () => {
   // service
   const storageService = new StorageService();
@@ -23,90 +23,95 @@ const Canvas = () => {
   const userID = useRecoilValue(userIdAtom);
 
   // state
-  const [text, setText] = useState<string>('');
-  const [fileURL, setFileURL] = useState<string | ArrayBuffer>('');
+  const [text, setText] = useState<string>('첫번째줄 입니다');
+  const [text2, setText2] = useState<string>('두번째줄 입니다');
   const [backImg, setBackImg] = useState<HTMLImageElement | undefined>();
   const [uploadImg, setUploadImg] = useState('');
   const { handleSubmit } = useForm();
 
   // ref
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
   const canvasTextRef = useRef<HTMLCanvasElement>(null);
 
   // custom function
-  const onUpdate = useCallback(
-    (url: string | ArrayBuffer) => setFileURL(url),
-    [],
-  );
-
-  const onTextChange: ReactEventHandler<HTMLInputElement> = e =>
-    setText(e.currentTarget.value);
-
-  const setImg = () => {
-    if (!fileURL) return;
+  const onUpdate = useCallback((url: string | ArrayBuffer) => {
     const img = new Image();
-    // 이미지 로딩 call back
     img.onload = () => {
-      if (!canvasRef.current) return;
       setBackImg(img);
     };
-    // url을 src넣기
-    img.src = fileURL as string;
+    img.src = url as string;
+  }, []);
+
+  const onTextChange: ReactEventHandler<HTMLInputElement> = ({
+    currentTarget,
+  }) => {
+    switch (currentTarget.name) {
+      case '첫줄':
+        setText(currentTarget.value);
+        break;
+      case '두번째줄':
+        setText2(currentTarget.value);
+        break;
+
+      default:
+        console.error('input state 저장 에러');
+        break;
+    }
   };
 
-  const drawImg = (ref: React.RefObject<HTMLCanvasElement>) => {
-    const ctx = ref.current?.getContext('2d');
-    ctx?.clearRect(0, 0, 300, 300);
-    if (!backImg) return;
-    ctx?.drawImage(backImg, 0, 0, 300, 300);
-  };
-
-  const drawText = (ref: React.RefObject<HTMLCanvasElement>, text: string) => {
+  const drawText = (ref: React.RefObject<HTMLCanvasElement>, y: number) => {
     if (!ref.current) return;
     const ctx = ref.current.getContext('2d');
     if (!ctx) return;
-    ctx.clearRect(0, 0, 300, 300);
+    if (backImg) {
+      ctx.drawImage(backImg, 0, 0, 500, 500);
+    } else {
+      ctx.clearRect(0, 0, 500, 500);
+    }
+    ctx.font = '36px Spoqa Han Sans Neo';
     ctx.textAlign = 'center';
-    ctx.font = '20px Aria';
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 1.3;
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'black';
-    ctx.fillText(text, 150, 230, 300);
-    ctx.strokeText(text, 150, 230, 300);
+    ctx.fillText(text, 250, y, 500);
+    ctx.strokeText(text, 250, y, 500);
+    ctx.fillText(text2, 250, y + 40, 500);
+    ctx.strokeText(text2, 250, y + 40, 500);
+  };
+
+  const drawImg = () => {
+    const ctx = canvasTextRef.current?.getContext('2d');
+    if (!backImg) return;
+    ctx?.drawImage(backImg, 0, 0, 500, 500);
   };
 
   const onUpload: SubmitHandler<any> = async (data: any) => {
     if (!canvasTextRef.current || !userID) return;
-    const ctx = canvasTextRef.current.getContext('2d');
-    if (!ctx || !backImg) return;
-    ctx.globalCompositeOperation = 'destination-over';
-    drawText(canvasTextRef, text);
-    ctx.drawImage(backImg, 0, 0, 300, 300);
-
     // 확인
     const save = confirm('저장합니다 아시겠어요?');
     if (!save) return;
+    drawImg();
+    drawText(canvasTextRef, 400);
     const base64 = canvasTextRef.current.toDataURL('image/jpeg');
-    setUploadImg(prev => {
-      if (prev === base64) {
-        alert('같은파일');
-        return prev;
-      } else {
-        return base64;
-      }
-    });
+    if (base64 === uploadImg) {
+      alert('같은파일입니다');
+      return;
+    }
+    setUploadImg(base64);
+    storageUpload(base64);
   };
 
   const reset = () => {
-    setText('');
-    setFileURL('');
     setBackImg(undefined);
+    setText('');
+    setText2('');
     setUploadImg('');
   };
 
-  const storageUpload = async () => {
+  const storageUpload = async (uploadImg: string) => {
     if (!userID) return;
     const results = await storageService.uploadImg(uploadImg, uuid());
+
     const imgURL = await getDownloadURL(results.ref);
     const imageData = {
       userID: userID,
@@ -121,62 +126,75 @@ const Canvas = () => {
   };
 
   useEffect(() => {
-    setImg();
-  }, [fileURL]);
-
-  useEffect(() => {
-    drawImg(canvasRef);
-  }, [backImg]);
-
-  useEffect(() => {
-    drawText(canvasTextRef, text);
-  }, [text]);
-
-  useEffect(() => {
-    storageUpload();
-  }, [uploadImg]);
+    drawText(canvasTextRef, 400);
+  }, [text, text2]);
 
   return (
-    <section className="flex w-full h-full">
-      <div className="relative shrink-0 basis-[350px] h-full mr-4 flex items-center flex-col">
-        <h1 className="text-center font-bold text-lg mb-2">프리뷰</h1>
-        <div className="relative w-[330px] h-[330px] border-2 border-rose-400 rounded">
-          <canvas
-            className="absolute top-[15px] left-[15px]"
-            ref={canvasRef}
-            width={300}
-            height={300}
-          ></canvas>
-          <canvas
-            className="absolute top-[15px] left-[15px]"
-            ref={canvasTextRef}
-            width={300}
-            height={300}
-          ></canvas>
+    <>
+      <h1 className="text-center font-bold text-2xl mb-4 font-myengjo">
+        짤 만들기
+      </h1>
+      <section className="flex flex-col w-full h-full md:flex-row">
+        <div className="relative shrink-0 basis-[350px] h-full mr-4 flex items-center flex-col">
+          <h2 className="text-center font-bold text-lg mb-2">미리보기</h2>
+          <div className="relative w-[500px] h-[500px] border-2 border-rose-400 rounded bg-black">
+            {backImg && (
+              <img
+                className="absolute top-0 left-0 w-full h-full z-0"
+                src={backImg?.src ?? ''}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null;
+                  currentTarget.src = 'images/default_logo.png';
+                }}
+                ref={imgRef}
+              />
+            )}
+
+            <canvas
+              className="absolute top-0 left-0 w-full h-full z-10"
+              ref={canvasTextRef}
+              width={500}
+              height={500}
+            ></canvas>
+          </div>
         </div>
-      </div>
-      <div className="w-full">
-        <h1 className="text-center font-bold text-lg mb-2">프리셋</h1>
-        <div className="border-2 border-rose-500 w-full p-2 rounded">
-          <form onSubmit={handleSubmit(onUpload)}>
-            <label htmlFor="text1">첫줄</label>
-            <input
-              type="text"
-              className="input"
-              onChange={onTextChange}
-              value={text}
-            />
-            <InputImg onUpdate={onUpdate} />
-            <button
-              className="btn-md btn-primary rounded-full py-1 mt-2 bg-rose-500"
-              type="submit"
-            >
-              저장하기
-            </button>
-          </form>
+        <div className="w-full">
+          <h2 className="text-center font-bold text-lg mb-2">프리셋</h2>
+          <div className="border-2 border-rose-500 w-full p-2 rounded">
+            <form onSubmit={handleSubmit(onUpload)}>
+              <label>
+                <span>첫줄</span>
+                <input
+                  type="text"
+                  name="첫줄"
+                  className="input"
+                  onChange={onTextChange}
+                  value={text}
+                />
+              </label>
+              <label>
+                <span>두번쨰 줄</span>
+                <input
+                  type="text"
+                  name="두번째줄"
+                  className="input"
+                  onChange={onTextChange}
+                  value={text2}
+                />
+              </label>
+
+              <InputImg onUpdate={onUpdate} />
+              <button
+                className="btn-md btn-primary rounded-full py-1 mt-2 bg-rose-500"
+                type="submit"
+              >
+                저장하기
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
